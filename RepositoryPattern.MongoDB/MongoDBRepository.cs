a9e5dd6aaa015eda45aacc0ba9d15382a021df6a
Raphael Beck <glitchedpolygons@gmail.com>
2019-04-23 00:36:17 +0200

    Implemented rest of base class methods + added xml docs.

--- RepositoryPattern.MongoDB/MongoDBRepository.cs
+++ RepositoryPattern.MongoDB/MongoDBRepository.cs
@@ -5,16 +5,15 @@ using System.Collections.Generic;
 
 using MongoDB.Bson;
 using MongoDB.Driver;
-using GlitchedPolygons.RepositoryPattern;
 
-namespace RepositoryPattern.MongoDB
+namespace GlitchedPolygons.RepositoryPattern.MongoDB
 {
     /// <summary>
     /// Abstract base class for MongoDB repositories.
     /// <seealso cref="IRepository{T1, T2}"/>
     /// </summary>
-    /// <typeparam name="TEntity">The type of entity that this repository will store.</typeparam>
-    public abstract class MongoDBRepository<TEntity> : IRepository<TEntity, ObjectId> where TEntity : IEntity<ObjectId>
+    /// <typeparam name="T">The type of entity that this repository will store.</typeparam>
+    public abstract class MongoDBRepository<T> : IRepository<T, ObjectId> where T : IEntity<ObjectId>
     {
         /// <summary>
         /// The underlying Mongo database.
@@ -24,9 +23,12 @@ namespace RepositoryPattern.MongoDB
         /// <summary>
         /// The underlying MongoDB collection.
         /// </summary>
-        protected readonly IMongoCollection<TEntity> collection;
+        protected readonly IMongoCollection<T> collection;
 
-        private readonly string typeName;
+        /// <summary>
+        /// The name of the repository's entity type.
+        /// </summary>
+        protected readonly string typeName;
 
         /// <summary>
         /// Creates a new MongoDB repository.
@@ -35,17 +37,77 @@ namespace RepositoryPattern.MongoDB
         protected MongoDBRepository(IMongoDatabase db)
         {
             this.db = db;
-            this.typeName = typeof(TEntity).Name;
-            collection = db.GetCollection<TEntity>(typeName);
+            this.typeName = typeof(T).Name;
+
+            collection = db.GetCollection<T>(typeName);
             if (collection is null)
             {
-                throw new MongoException($"{nameof(MongoDBRepository<TEntity>)}::ctor: No collection named \"{typeName}\" found in database!");
+                throw new MongoException($"{nameof(MongoDBRepository<T>)}::ctor: No collection named \"{typeName}\" found in database!");
             }
         }
 
-        public TEntity this[ObjectId id] => collection.Find(u => u.Id == id).FirstOrDefault();
+        #region Get
 
-        public async Task<bool> Add(TEntity entity)
+        /// <summary>
+        /// Synchronously gets an entity by its unique identifier.
+        /// </summary>
+        /// <param name="id">The entity's unique identifier.</param>
+        /// <returns>The found entity; <c>null</c> if the entity couldn't be found.</returns>
+        public T this[ObjectId id] => collection.Find(u => u.Id == id).FirstOrDefault();
+
+        /// <summary>
+        /// Gets an entity by its unique identifier.
+        /// </summary>
+        /// <param name="id">The entity's unique identifier.</param>
+        /// <returns>The first found <see cref="T:GlitchedPolygons.RepositoryPattern.IEntity`1" />; <c>null</c> if nothing was found.</returns>
+        public async Task<T> Get(ObjectId id)
+        {
+            IAsyncCursor<T> cursor = await collection.FindAsync(u => u.Id == id);
+            return await cursor.FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Gets all entities from the repository.
+        /// </summary>
+        /// <returns>All entities inside the repo.</returns>
+        public Task<IEnumerable<T>> GetAll()
+        {
+            IEnumerable<T> r = collection.AsQueryable().ToEnumerable();
+            return Task.FromResult(r);
+        }
+
+        /// <summary>
+        /// Finds all entities according to the specified predicate <see cref="T:System.Linq.Expressions.Expression" />.
+        /// </summary>
+        /// <param name="predicate">The search predicate (all entities that match the provided conditions will be added to the query's result).</param>
+        /// <returns>The found entities (<see cref="T:System.Collections.Generic.IEnumerable`1" />).</returns>
+        public async Task<IEnumerable<T>> Find(Expression<Func<T, bool>> predicate)
+        {
+            return (await collection.FindAsync(predicate)).ToEnumerable();
+        }
+
+        /// <summary>
+        /// Gets a single entity from the repo according to the specified predicate condition.<para></para>
+        /// If 0 or &gt;1 entities are found, <c>null</c> is returned.
+        /// </summary>
+        /// <param name="predicate">The search predicate.</param>
+        /// <returns>Single found entity; <c>null</c> if 0 or &gt;1 entities were found.</returns>
+        public async Task<T> SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            IAsyncCursor<T> cursor = await collection.FindAsync(predicate);
+            return await cursor.SingleOrDefaultAsync();
+        }
+
+        #endregion
+
+        #region Add
+
+        /// <summary>
+        /// Adds the specified entity to the data repository.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>Whether the entity could be added successfully or not.</returns>
+        public async Task<bool> Add(T entity)
         {
             try
             {
@@ -58,7 +120,12 @@ namespace RepositoryPattern.MongoDB
             }
         }
 
-        public async Task<bool> AddRange(IEnumerable<TEntity> entities)
+        /// <summary>
+        /// Adds multiple entities at once.
+        /// </summary>
+        /// <param name="entities">The entities to add.</param>
+        /// <returns>Whether the entities were added successfully or not.</returns>
+        public async Task<bool> AddRange(IEnumerable<T> entities)
         {
             try
             {
@@ -71,62 +138,117 @@ namespace RepositoryPattern.MongoDB
             }
         }
 
-        public async Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity, bool>> predicate)
-        {
-            return (await collection.FindAsync(predicate)).ToEnumerable();
-        }
+        #endregion
 
-        public async Task<TEntity> Get(ObjectId id)
-        {
-            IAsyncCursor<TEntity> cursor = await collection.FindAsync(u => u.Id == id);
-            return await cursor.FirstOrDefaultAsync();
-        }
+        #region Remove
 
-        public Task<IEnumerable<TEntity>> GetAll()
-        {
-            IEnumerable<TEntity> r = collection.AsQueryable().ToEnumerable();
-            return Task.FromResult(r);
-        }
-
-        public async Task<bool> Remove(TEntity entity)
+        /// <summary>
+        /// Removes the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity to remove.</param>
+        /// <returns>Whether the entity could be removed successfully or not.</returns>
+        public async Task<bool> Remove(T entity)
         {
             var r = await collection.DeleteOneAsync(u => u.Id == entity.Id);
             return r.IsAcknowledged;
         }
 
+        /// <summary>
+        /// Removes the specified entity.
+        /// </summary>
+        /// <param name="id">The unique id of the entity to remove.</param>
+        /// <returns>Whether the entity could be removed successfully or not.</returns>
         public async Task<bool> Remove(ObjectId id)
         {
             var r = await collection.DeleteOneAsync(u => u.Id == id);
             return r.IsAcknowledged;
         }
 
-        public Task<bool> RemoveAll()
+        /// <summary>
+        /// Removes all entities at once from the repository.
+        /// </summary>
+        /// <returns>Whether the entities were removed successfully or not. If the repository was already empty, <c>false</c> is returned (because nothing was actually &lt;&lt;removed&gt;&gt; ).</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public async Task<bool> RemoveAll()
         {
-            throw new NotImplementedException();
+            var r = await collection.DeleteManyAsync(FilterDefinition<T>.Empty);
+            return r.IsAcknowledged;
         }
 
-        public async Task<bool> RemoveRange(Expression<Func<TEntity, bool>> predicate)
+        /// <summary>
+        /// Removes all entities that match the specified conditions (via the predicate <see cref="T:System.Linq.Expressions.Expression" /> parameter).
+        /// </summary>
+        /// <param name="predicate">The predicate <see cref="T:System.Linq.Expressions.Expression" /> that defines which entities should be removed.</param>
+        /// <returns>Whether the entities were removed successfully or not.</returns>
+        public async Task<bool> RemoveRange(Expression<Func<T, bool>> predicate)
         {
             var r = await collection.DeleteManyAsync(predicate);
             return r.IsAcknowledged;
         }
 
-        public Task<bool> RemoveRange(IEnumerable<TEntity> entities)
+        /// <summary>
+        /// Removes the range of entities from the repository.
+        /// </summary>
+        /// <param name="entities">The entities to remove.</param>
+        /// <returns>Whether the entities were removed successfully or not.</returns>
+        public async Task<bool> RemoveRange(IEnumerable<T> entities)
         {
-            throw new NotImplementedException();
+            if (entities is null)
+            {
+                return false;
+            }
+
+            bool success = true;
+            var tasks = new List<Task>(8);
+            foreach (T entity in entities)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    if (!collection.DeleteOne(u => u.Id == entity.Id).IsAcknowledged)
+                    {
+                        success = false;
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+            return success;
         }
 
-        public Task<bool> RemoveRange(IEnumerable<ObjectId> ids)
+        /// <summary>
+        /// Removes the range of entities from the repository.
+        /// </summary>
+        /// <param name="ids">The unique ids of the entities to remove.</param>
+        /// <returns>Whether all entities were removed successfully or not.</returns>
+        public async Task<bool> RemoveRange(IEnumerable<ObjectId> ids)
         {
-            throw new NotImplementedException();
+            if (ids is null)
+            {
+                return false;
+            }
+
+            bool success = true;
+            var tasks = new List<Task>(8);
+            foreach (ObjectId id in ids)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    if (!collection.DeleteOne(u => u.Id == id).IsAcknowledged)
+                    {
+                        success = false;
+                    }
+                }));
+            }
+            await Task.WhenAll(tasks);
+            return success;
         }
 
-        public async Task<TEntity> SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
-        {
-            IAsyncCursor<TEntity> cursor = await collection.FindAsync(predicate);
-            return await cursor.SingleOrDefaultAsync();
-        }
+        #endregion
 
-        public abstract Task<bool> Update(TEntity entity);
+        /// <summary>
+        /// Updates the specified entity.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>Whether the entity could be updated successfully or not.</returns>
+        public abstract Task<bool> Update(T entity);
     }
 }
